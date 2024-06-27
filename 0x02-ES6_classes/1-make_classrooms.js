c1647e5d4e825cd54202f30b4a481d48a57fcb07
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-06-28 00:46:01 +0300

    check

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -4,7 +4,7 @@ export default function initializeRooms() {
   const rooms = [];
   const maxStudentsSize = [19, 20, 30];
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i += 1) {
     rooms.push(new ClassRoom(maxStudentsSize[i]));
   }
   return rooms;
