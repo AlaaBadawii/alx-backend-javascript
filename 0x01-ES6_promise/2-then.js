fc8f668fe96803da35e7a6a53c4eb47db2426885
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-06-28 23:06:17 +0300

    recheck and fix  2-then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,12 +2,13 @@ function handleResponseFromAPI(promise) {
   promise
     .then(() => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
+      return {
+        status: 200,
+        body: 'success',
+      };
     })
     .catch(() => {
       console.log('Got a response from the API');
       return new Error();
     });
 }
-
-export default handleResponseFromAPI;
