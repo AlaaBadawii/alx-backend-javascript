b7b2d5ba11fb3bbcf17d474531a5c299e6091553
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-06-29 02:01:55 +0300

    check 6-final-user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,13 @@
-import signUpUser from './5-photo-reject';
-import uploadPhoto from './4-user-promise';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      }
-      return {
-        status: result.status,
-        value: result.reason,
-      };
-    }));
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
