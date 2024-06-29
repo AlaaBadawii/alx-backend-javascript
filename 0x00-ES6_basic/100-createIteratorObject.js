cbf369eb69bdb22f325c781722994ff554d7f38d
AlaaBadawii <alaaahmed16112000@gmail.com>
2024-06-29 03:11:07 +0300

    check

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,15 +1,16 @@
 export default function createIteratorObject(report) {
-    return {
-        [Symbol.iterator]() {
-        let i = 0;
-        return {
-            next() {
-            if (i < report.length) {
-                return { value: report[i++], done: false };
-            }
-            return { value: undefined, done: true };
-            }
-        };
-        }
-    };
+  return {
+    [Symbol.iterator]() {
+      let i = 0;
+      return {
+        next() {
+          if (i < report.length) {
+            const ret = { value: report[i += 1], done: false };
+            return ret;
+          }
+          return { value: undefined, done: true };
+        },
+      };
+    },
+  };
 }
