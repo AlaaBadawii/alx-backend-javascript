f665e218e7631667b203b9ceec9170894de4c154
Alaa Badawii <alaa.badawy404@gmail.com>
2026-03-28 06:28:58 +0200

    check styles

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,16 +1,8 @@
-function getFullResponseFromAPI(success) {
-  const myPromise = new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(Error('The fake API is not working currently'));
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      return resolve({ status: 200, body: 'success' });
     }
+    return reject(new Error('The fake API is not working currently'));
   });
-
-  return myPromise;
 }
-
-export default getFullResponseFromAPI;
